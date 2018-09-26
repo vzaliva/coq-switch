96844155302d5c5c649d1bd45152c21f327bb891
Vadim Zaliva <lord@crocodile.org>
2018-09-26 17:53:04 -0500

    initial implementation for natural numbers

---
+++ README.md
@@ -0,0 +1,15 @@
+# coq-switch #
+
+`coq-switch` plugin for Coq
+
+### Dependencies ###
+
+* [Coq 8.8.1](https://coq.inria.fr/) 
+* [Template Coq](https://template-coq.github.io/template-coq/)
+
+To install all required dependenceis:
+
+    opam repo add coq-released https://coq.inria.fr/opam/released
+    opam install --jobs=4 coq coq-template-coq
+
+Questions: [Vadim Zaliva](mailto:vzaliva@cmu.edu)
---
+++ switch.v
@@ -0,0 +1,94 @@
+Require Import Coq.Strings.String.
+Require Import Coq.Lists.List.
+Require Import Template.All.
+
+Import MonadNotation.
+Import ListNotations.
+
+Fixpoint mkSwitchCases
+         (type_name: string)
+         (A_t: term)
+         (P_t: term)
+         (choices_t: list term)
+         (n: nat) : term :=
+  let ind_0 n := {| inductive_mind := n; inductive_ind := 0 |} in
+  let T_i := ind_0 type_name in
+  let bool_i := ind_0 "Coq.Init.Datatypes.bool" in
+  let opt_i := ind_0 "Coq.Init.Datatypes.option" in
+  match choices_t with
+  | [] => tApp (tConstruct opt_i 1 []) [tInd T_i []]
+
+  | (x::xs) => tCase
+                (* # of parameters *)
+                (bool_i, 0)
+
+                (* type info *)
+                (tLambda (nNamed "b") (tInd bool_i []) (tApp (tInd opt_i []) [tInd T_i []]))
+
+                (* discriminee *)
+                (tApp P_t [tRel 0; x])
+
+                (* branches *)
+                [
+                  (0, tApp (tConstruct opt_i 0 []) [tInd T_i []; tConstruct T_i n []]) ;
+                    (0, mkSwitchCases type_name A_t P_t xs (S n))
+                ]
+  end.
+
+Definition mkSwitch
+           (A: Type)
+           (P: A -> A -> bool)
+           (choices: list (A*string))
+           (type_name: string) (def_name: string): TemplateMonad unit
+  :=
+    let one_i : one_inductive_entry :=
+        {|
+          mind_entry_typename := type_name ;
+          mind_entry_arity := tSort Universe.type0 ;
+          mind_entry_template := false ;
+          mind_entry_consnames := map snd choices ;
+          mind_entry_lc := map (fun _ => tRel 0) choices;
+        |} in
+    let ind:mutual_inductive_entry := {|
+          mind_entry_record    := None ;
+          mind_entry_finite    := Finite ;
+          mind_entry_params    := [] ;
+          mind_entry_inds      := [one_i] ;
+          mind_entry_universes := Monomorphic_ctx ([], ConstraintSet.empty) ;
+          mind_entry_private   := None (* or (Some false)? *)
+        |} in
+    ind_t <- tmMkInductive ind ;;
+          T <- tmUnquoteTyped Type (tInd {| inductive_mind := type_name; inductive_ind := 0 |} []) ;;
+          A_t <- tmQuote A ;;
+          P_t <- tmQuote P ;;
+          choices_t <- monad_map (fun x => tmQuote (fst x)) choices ;;
+          let body_t := tLambda (nNamed "x") A_t (mkSwitchCases type_name A_t P_t choices_t 0) in
+          body <- tmUnquoteTyped (A -> option T) body_t ;;
+               def_t <- tmDefinition def_name body ;;
+               tmReturn tt.
+
+
+(* Samples below *)
+
+Require Import Coq.Arith.EqNat.
+
+Section NatExample.
+  Run TemplateProgram
+      (mkSwitch nat
+                beq_nat
+                [(0,"Love") ; (10,"Ten") ; (20, "twenty")]
+                "Ex1_Choices" "ex1_select"
+      ).
+
+  Print Ex1_Choices.
+  Print ex1_select.
+
+
+  Definition Foo (n:nat) (p:Ex1_Choices) : nat :=
+    match ex1_select n with
+    | Some Love => 100
+    | Some Ten => 110
+    | Some Twenty => 120
+    | None => 0
+    end.
+End NatExample.
