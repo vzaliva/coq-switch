4728dc088ecaca1dfc8013b8980ce645fe0bc204
Vadim Zaliva <lord@crocodile.org>
2018-09-26 18:02:53 -0500

    refactoring

---
+++ demo.v
@@ -0,0 +1,58 @@
+Require Import switch.
+
+Require Import Coq.Arith.EqNat.
+Require Import Coq.Strings.String.
+Require Import Coq.Lists.List.
+Require Import Template.All.
+
+Import ListNotations.
+
+Section NatExample.
+
+  Run TemplateProgram
+      (mkSwitch nat
+                beq_nat
+                [(0,"Love") ; (10,"Ten") ; (20, "twenty")]
+                "Ex1_Choices" "ex1_select"
+      ).
+
+  Print Ex1_Choices.
+  Print ex1_select.
+
+
+  Definition Ex1 (n:nat) (p:Ex1_Choices) : nat :=
+    match ex1_select n with
+    | Some Love => 100
+    | Some Ten => 110
+    | Some Twenty => 120
+    | None => 0
+    end.
+
+End NatExample.
+
+
+Section StringExample.
+
+  Definition string_beq a b :=
+    if string_dec a b then true else false.
+
+  Run TemplateProgram
+      (mkSwitch string
+                string_beq
+                [("love","SLove") ; ("ten","STen") ; ("twenty","STwenty")]
+                "Ex2_Choices" "ex2_select"
+      ).
+
+  Print Ex2_Choices.
+  Print ex2_select.
+
+
+  Definition Ex2 (s:string) (p:Ex2_Choices) : nat :=
+    match ex2_select s with
+    | Some SLove => 100
+    | Some STen => 110
+    | Some STwenty => 120
+    | None => 0
+    end.
+
+End StringExample.
